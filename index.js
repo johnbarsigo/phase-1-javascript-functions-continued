9da2834b485d9a39242db8d502c664bbdacb6d26
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-19 06:01:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun ( activity ){
+    if ( activity == null ){
+        return `This Saturday, I want to roller-skate!`
+    }
+    else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+ //console.log(saturdayFun( 'drive' ))
+
+function mondayWork ( activity ){
+    if ( activity == null ){
+        return `This Monday, I will go to the office.`
+    }
+    else {
+        return `This Monday, I will ${activity}.`
+    }
+}
+
+//console.log( mondayWork() )
+
+function wrapAdjective ( flair ){
+    let encouragingText
+    return function ( adj = 'special' ){
+        return `You are ${flair}${adj}${flair}!`
+        }
+    }
